bf05728d8117ed2a58eeb6b819a0b85a5c62bb21
njleonzhang <scream3616@sina.com>
2017-10-10 16:13:20 +0800

    upgrade

---
+++ README.md
@@ -0,0 +1,10 @@
+# my-react-starter
+webpack react starter refer to Vue webpack
+
+* webpack3
+* Mobx 3
+* react-router 4
+* stylus | scss | sass support
+* hot reload
+
+> hot realod with `react-router` + `mobx` can not be implemented by configuration. in this project, we abandon to keep `current store` after hot reload. but the update to store(both value and action) can work. 
---
+++ src/stores/stores/homeStore.js
@@ -0,0 +1,25 @@
+import { observable, action } from 'mobx'
+import api from '@api'
+
+export class HomeStore {
+  @observable devices = []
+
+  @action loadHomeData() {
+    this.devices = []
+
+    return api.Device.getDevices({ throwError: true })
+      .then(data => {
+        this.devices = data
+        console.log(this.devices)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  @action clear() {
+    this.devices = []
+  }
+}
+
+export default new HomeStore()
